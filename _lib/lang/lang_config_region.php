8b04fec8435e02b0b91a056530c57a665cf5dafd
JP <ingejp@outlook.com>
2020-06-04 16:55:33 -0700

    version completa

---
+++ _lib/lang/lang_config_region.php
@@ -0,0 +1,6 @@
+<?php
+  $this->Nm_lang_conf_region = array();
+  $this->Nm_lang_conf_region['en_us;es_gt'] = "Español (Guatemala)";
+  $this->Nm_lang_conf_region['es;es_gt'] = "Español (Guatemala)";
+  ksort($this->Nm_lang_conf_region);
+?>
